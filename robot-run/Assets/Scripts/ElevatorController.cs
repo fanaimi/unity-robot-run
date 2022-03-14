12499134c884bd288d64ac003907d22bdc244e56
fanaimi <fanaimi@users.noreply.github.com>
2022-03-14 19:54:10 +0000

    elevator prototype

--- robot-run/Assets/Scripts/ElevatorController.cs
+++ robot-run/Assets/Scripts/ElevatorController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ElevatorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float m_TimeFromStart = 0f;
+    private float m_Interval = 3f;
+    private float m_Speed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_TimeFromStart += Time.deltaTime;
+
+        if (m_TimeFromStart > m_Interval)
+        {
+            m_TimeFromStart = 0;
+            m_Speed = -m_Speed;
+        }
+        else
+        {
+            transform.Translate(0, m_Speed* Time.deltaTime,0);
+        }
     }
 }
